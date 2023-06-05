03c3ee0216cc4a6c90d895dd5821ed86d69b6c78
AdilTehranli <adiljt@code.edu.az>
2023-06-05 15:21:49 +0400

    Crated

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict "
+
+function show(){
+    let inp=document.getElementById('inp');
+    let icon=document.getElementById('icon');
+
+    if(inp.type==='password'){
+        inp.type='text';
+        icon.className='fa-solid fa-eye-slash'
+        
+    }else{
+        inp.type='password';
+        icon.className='fa-solid fa-eye'
+        
+        
+    }
+}
